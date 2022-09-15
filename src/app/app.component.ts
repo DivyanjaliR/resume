0a7f428cae940172c40e52572a1b82d7f22c2f36
DivyanjaliR <188r1a05b1@gmail.com>
2022-09-15 17:08:44 +0530

    resume

---
+++ src/app/app.component.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+import studentsData from '../assets/students.json';
+import resumeData from '../assets/resume.json';
+
+
+interface Student {  
+  id: Number;  
+  name: String;  
+  email: String;  
+  gender: String;  
+}  
+
+interface Education {
+  title : String;
+  name : String; 
+  description: String;
+}
+
+interface Experiance {
+  title : String;
+  name : String; 
+  description: String;
+}
+
+interface Project {
+  name : String; 
+  description: String;
+}
+
+interface Resume {
+  fullName:String; 
+  address: String; 
+  email: String; 
+  github:String;
+  mobileNo:String; 
+  description:String; 
+  EducProfile: Education[];
+  Work: Experiance[];
+  personalProjects: Project[];
+  languages: String[];
+  interest: String[];
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'demo-app'; 
+  students: Student[] = studentsData; 
+  resume: Resume = resumeData[0];  
+}
